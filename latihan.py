3ec05060b0043273316fe91bb7bf51ba712c3b31
Amanda Neisya <mndnsy@gmail.com>
2026-03-10 12:25:08 +0700

    Upload file latihan pemrograman 3

---
+++ latihan.py
@@ -0,0 +1,13 @@
+n1 = int(input ("Nilai ke-1 : "))
+n2 = int(input ("Nilai ke-2 : "))
+n3 = int(input ("Nilai ke-3 : "))
+
+jumlah = n1 + n2 + n3
+kali = n1 * n2 * n3
+bagi = n1 / n2 / n3
+kurang = n1 - n2 - n3
+
+print("hasil penjumlahan = ", jumlah)
+print("hasil perkalian = ", kali)
+print("hasil pembagian = ", bagi)
+print("hasil pengurangan = ", kurang)
